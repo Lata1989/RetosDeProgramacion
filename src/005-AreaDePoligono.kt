bf22f1d42fde7822f0591ae426c8daa163db7d7d
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2024-08-23 18:03:34 -0300

    poligonos listo!

--- src/005-AreaDePoligono.kt
+++ src/005-AreaDePoligono.kt
@@ -6,5 +6,24 @@ fun main() {
         * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
         * - Imprime el cálculo del área de un polígono de cada tipo.
     */
+    var base: Double = 0.0
+    var altura: Double = 0.0
 
+    do {
+        print("Ingrese el valor de la base:")
+        base = readln().toDouble()
+    } while (base < 1)
+    do {
+        print("Ingrese el valor de la altura:")
+        altura = readln().toDouble()
+    } while (altura < 1 || base == altura)
+
+    var areaTriangulo = base * altura / 2
+    println("El area del triangulo es: ${areaTriangulo}")
+
+    var areaCuadrado = base * base
+    println("El area del cuadrado es: ${areaCuadrado}")
+
+    var areaRectangulo = base * altura
+    println("El area del rectangulo es: ${areaRectangulo}")
 }
